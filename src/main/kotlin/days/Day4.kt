8d668949856e5327c532c8c060c671b140829b98
Andi Lau <andi@herrlau.de>
2021-12-04 20:41:05 +0100

    Mod Day4 "Giant Squid" (improvements)

--- src/main/kotlin/days/Day4.kt
+++ src/main/kotlin/days/Day4.kt
@@ -6,22 +6,24 @@ package days
     date = Date(day = 4, year = 2021)
 )
 class Day4(input: List<String>) : Puzzle {
-    private val draws = input.first().split(',').map(String::toInt)
+    private val draws = readDraws(input)
     private val boards = readBoards(input)
 
-    override fun partOne(): Int =
+    override fun partOne() =
         draws.firstNotNullOf { draw ->
             boards
                 .firstOrNull { board -> board.markForBingo(draw) }
                 ?.let { draw * it.unmarked().sum() }
         }
 
-    override fun partTwo(): Int =
+    override fun partTwo() =
         boards
             .associateWith { board -> draws.first { board.markForBingo(it) } }
             .maxByOrNull { draws.indexOf(it.value) }
             ?.let { it.key.unmarked().sum() * it.value }
             ?: error("")
+
+    private fun readDraws(input: List<String>) = input.first().split(',').map(String::toInt)
 
     private fun readBoards(input: List<String>) = input
         .asSequence()
@@ -53,27 +55,21 @@ class Day4(input: List<String>) : Puzzle {
 
         companion object {
             fun from(input: String): BingoBoard {
-                return BingoBoard(input
-                    .trim()
-                    .split(" ")
-                    .filter { it.isNotEmpty() }
-                    .map(String::toInt)
-                    .toList())
+                return BingoBoard(
+                    input
+                        .trim()
+                        .split(whitespaces)
+                        .map(String::toInt)
+                        .toList()
+                )
             }
 
-            private val winnerRanges = listOf(
-                0..4,
-                5..9,
-                10..14,
-                15..19,
-                20..24,
-                0..24 step 5,
-                1..24 step 5,
-                2..24 step 5,
-                3..24 step 5,
-                4..24 step 5,
-            )
+            private val whitespaces = Regex("\\s+")
 
+            private val winnerRanges = listOf(
+                0..4, 5..9, 10..14, 15..19, 20..24,
+                0..24 step 5, 1..24 step 5, 2..24 step 5, 3..24 step 5, 4..24 step 5,
+            )
         }
     }
 }
--- src/test/kotlin/days/Day4Test.kt
+++ src/test/kotlin/days/Day4Test.kt
@@ -8,36 +8,36 @@ import org.junit.jupiter.api.Test
 @DisplayName("Day 4")
 class Day4Test {
     private val input =
-        """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+        """
+            7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
-22 13 17 11  0
- 8  2 23  4 24
-21  9 14 16  7
- 6 10  3 18  5
- 1 12 20 15 19
-
- 3 15  0  2 22
- 9 18 13 17  5
-19  8  7 25 23
-20 11 10 24  4
-14 21 16 12  6
-
-14 21 17 24  4
-10 16 15  9 19
-18  8 23 26 20
-22 11 13  6  5
- 2  0 12  3  7
+            22 13 17 11  0
+             8  2 23  4 24
+            21  9 14 16  7
+             6 10  3 18  5
+             1 12 20 15 19
+            
+             3 15  0  2 22
+             9 18 13 17  5
+            19  8  7 25 23
+            20 11 10 24  4
+            14 21 16 12  6
+            
+            14 21 17 24  4
+            10 16 15  9 19
+            18  8 23 26 20
+            22 11 13  6  5
+             2  0 12  3  7
         """.trimIndent().lines()
 
     @Nested
     @DisplayName("Part 1")
     inner class Part1 {
-
         @Test
-        fun exxample() {
+        fun firstBoardToWinShouldWinOnDraw24AndTotalOf188UnmarkedNumbers() {
             val sumUnmarked = 188
-            val lastNumber = 24
-            assertThat(Day4(input).partOne()).isEqualTo(sumUnmarked * lastNumber)
+            val onDraw = 24
+            assertThat(Day4(input).partOne()).isEqualTo(sumUnmarked * onDraw)
         }
     }
 
@@ -45,8 +45,10 @@ class Day4Test {
     @DisplayName("Part 2")
     inner class Part2 {
         @Test
-        fun example() {
-            assertThat(Day4(input).partTwo()).isEqualTo(148 * 13)
+        fun lastBoardToWinShouldWinOnDraw13AndTotalOf148UnmarkedNumbers() {
+            val sumUnmarked = 148
+            val onDraw = 13
+            assertThat(Day4(input).partTwo()).isEqualTo(sumUnmarked * onDraw)
         }
     }
 }
