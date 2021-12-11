bd9e05aff83ccd94beb8e420067b500ac87e6cc6
Andi Lau <andi@herrlau.de>
2021-12-11 16:53:06 +0100

    Add Day11 "Dumbo Octopus" fix

--- src/test/kotlin/days/Day11Test.kt
+++ src/test/kotlin/days/Day11Test.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-@DisplayName("Day 10")
+@DisplayName("Day 11")
 class Day11Test {
 
     val smallExampleSequence = listOf(
