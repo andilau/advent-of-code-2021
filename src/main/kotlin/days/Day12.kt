15c15bd4728627f02fb846b7e882da98a860ea81
Andi Lau <andi@herrlau.de>
2021-12-12 16:59:34 +0100

    Add Day12 "Passage Pathing" cleanup

--- src/main/kotlin/days/Day12.kt
+++ src/main/kotlin/days/Day12.kt
@@ -7,68 +7,55 @@ package days
 )
 class Day12(val input: List<String>) : Puzzle {
 
-    private val nodes = input.flatMap { line ->
-        val (from, to) = line.split('-')
-        buildList {
-            if (from != END && to != START) add(from to to)
-            if (from != START && to != END) add(to to from)
-        }
-    }.groupBy(keySelector = { it.first }, valueTransform = { it.second })
+    private val links = input.parseInput()
 
-    override fun partOne(): Int {
-        var paths: MutableSet<List<String>> = nodes.getValue("start").map { listOf("start", it) }.toMutableSet()
+    override fun partOne() =
+        links.step { path, next -> next !in path }.size
+
+    override fun partTwo() =
+        links.step { path, next ->
+            next !in path ||
+                    path.filter(String::isSmallCave).groupBy { it }
+                        .filterValues { it.size == 2 }
+                        .isEmpty()
+        }
+            .size
+
+    private inline fun Map<String, List<String>>.step(
+        visitBig: (List<String>, String) -> Boolean = { _, _ -> true },
+        visitSmall: (List<String>, String) -> Boolean
+    ): Set<List<String>> {
+        var paths: MutableSet<List<String>> = this.getValue("start").map { listOf("start", it) }.toMutableSet()
         while (true) {
             val new = mutableSetOf<List<String>>()
-
             for (path in paths) {
                 val last = path.last()
                 if (last == END) {
                     new.add(path)
-                    continue
                 }
-                val all = nodes.getValue(last)
-                for (next in all) {
-                    if (!next.isSmallCave())
-                        new.add(path + next)
-                    else if (next !in path)
-                        new.add(path + next)
-                }
-            }
-
-            paths = new
-            if (new.map { it.last() }.all { it == END }) break
-        }
-        return paths.size
-    }
-
-    override fun partTwo(): Int {
-        var paths: MutableSet<List<String>> = nodes.getValue("start").map { listOf("start", it) }.toMutableSet()
-
-        while (true) {
-            val new = mutableSetOf<List<String>>()
-
-            for (path in paths) {
-                val last = path.last()
-                if (last == END) {
-                    new.add(path); continue
-                }
-                val all = nodes.getValue(last)
-                for (next in all) {
-                    if (!next.isSmallCave()) {
-                        new.add(path + next)
-                    }
-                    else
-                        if (next !in path || path.filter(String::isSmallCave).groupBy { it }
-                                .filterValues { it.size == 2 }.isEmpty())
+                else {
+                    this.getValue(last).forEach { next ->
+                        if (next.isBigCave() && visitBig.invoke(path, next))
                             new.add(path + next)
+                        if (next.isSmallCave() && visitSmall.invoke(path, next))
+                            new.add(path + next)
+                    }
                 }
             }
             paths = new
             if (new.map { it.last() }.all { it == END }) break
         }
-        return paths.size
+        return paths.toSet()
     }
 
+    private fun List<String>.parseInput(): Map<String, List<String>> =
+        flatMap { line ->
+            val (from, to) = line.split('-')
+            buildList {
+                if (from != END && to != START) add(from to to)
+                if (from != START && to != END) add(to to from)
+            }
+        }.groupBy(keySelector = { it.first }, valueTransform = { it.second })
 
     companion object {
         const val START = "start"
