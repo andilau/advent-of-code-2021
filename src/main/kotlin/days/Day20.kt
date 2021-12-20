706e4c8fa7d8d68faceadb655e39243edd2eab79
Andi Lau <andi@herrlau.de>
2021-12-20 13:04:45 +0100

    Add Day20 try

---
+++ src/main/kotlin/days/Day20.kt
@@ -0,0 +1,60 @@
+package days
+
+@AdventOfCodePuzzle(
+    name = "Beacon Scanner",
+    url = "https://adventofcode.com/2021/day/20",
+    date = Date(day = 20, year = 2021)
+)
+class Day20(val input: List<String>) : Puzzle {
+    private val algorithm = input[0].map { if (it == '#') 0 else 1 }.toIntArray()
+    private val inv = algorithm[0] == 1
+    var data = input.drop(2).map { it.map { if (it == '#') 1 else 0 }.toIntArray() }.toTypedArray()
+
+    class Image(val data: Array<IntArray>) {
+        private val height = data.size
+        private val width = data[0].size
+
+        fun enhance(algorithm: IntArray): Image {
+            val b = Array(height + 2) { IntArray(width + 2) }
+            val bg = 0
+            for (i in 0 until height + 2) for (j in 0 until width + 2) {
+                var s = 0
+                for (i1 in -1..1) for (j1 in -1..1) {
+                    s = (s shl 1) + when (data.getOrNull(i + i1 - 1)?.getOrNull(j + j1 - 1)) {
+                        null -> 0
+                        1 -> 1
+                        0 -> 0
+                        else -> error("")
+                    }
+                }
+                b[i][j] = algorithm[s] xor 1
+            }
+            return Image(b)
+        }
+
+        fun pixels() = data.sumOf { it.sum() }
+
+        fun print() {
+            println(data.joinToString("\n") {
+                it.map {
+                    when (it) {
+                        0 -> '.';1 -> '#';else -> error("")
+                    }
+                }.joinToString("")
+            })
+        }
+    }
+
+    override fun partOne(): Int {
+        return Image(data)
+            .also { it.print() }
+            .enhance(algorithm)
+            .also { it.print() }
+            .enhance(algorithm)
+            .also { it.print() }.pixels()
+    }
+
+    override fun partTwo(): Any {
+        return 0
+    }
+}
---
+++ src/test/kotlin/days/Day20Test.kt
@@ -0,0 +1,37 @@
+package days
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("Day 20")
+class Day20Test {
+    val example = """..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###
+""".lines()
+
+
+    @Nested
+    @DisplayName("Part 1")
+    inner class Part1 {
+        @Test
+        fun `After enhancing the original input image twice, 35 pixels are lit`() {
+            assertThat(Day20(example).partOne()).isEqualTo(35)
+        }
+    }
+
+    @Nested
+    @DisplayName("Part 2")
+    inner class Part2 {
+        @Test
+        fun `The largest Manhattan distance between any two scanners should equal 3621`() {
+
+        }
+    }
+}
