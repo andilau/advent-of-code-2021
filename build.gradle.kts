a9fbf5c9f4f7607831252cd75fb2c10fc71c75f7
Andi Lau <andi@herrlau.de>
2022-04-13 21:29:25 +0200

    Upgrade to Kotlin 1.6.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.6.20"
 }
 
 application {
@@ -16,8 +16,8 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation("org.reflections:reflections:0.10.2")
     implementation("org.slf4j:slf4j-nop:1.7.35")
